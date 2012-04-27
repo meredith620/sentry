1ac5dbec0e2afd9345abacb878246624c2e9ea9d
garfee <garfee@me.com>
2012-04-27 22:57:18 +0800

    add all.html

--- test/js-gchart/web/index.js
+++ test/js-gchart/web/index.js
@@ -2,6 +2,55 @@ var http=require('http');
 var url=require('url');
 var querystring=require('querystring');
 var ds=require('./data');
+var fs=require('fs');
+var cp=require('cp');
+
+//TODO grantee node_root exist
+global.node_root='./node_root';
+global.node=[];
+function node_list(response,param){
+	fs.readdir(node_root,function(err,files){
+		if(err){
+			throw err;
+		}
+		global.node=files;
+		console.log(files);
+		response.end(JSON.stringify(files));
+	});
+}
+
+function node_add(response,param){
+	console.log(param);
+	if(param.host){
+		response.end();
+		return;
+	}
+	deploy_islet(param.host,function(){
+		response.end('add server success');
+	});
+}
+
+function deploy_islet(host,cb){
+	var deploy_cmd='scp -r ./islet '+host+':~/';
+	var run_cmd='ssh '+host+' cd islet && ./catcher.py &>log &';
+	cp.exec(deploy_cmd,function(err,stdout,stderr){
+		if(err){
+			throw err;
+		}
+		cp.exec(run_cmd,function(err,stdout,stderr){
+			if(err){
+				throw err;
+			}
+			fs.mkdir(node_root+'/'+host,function(err){
+				if(err){
+					throw err;
+				}
+				cb();
+			});
+		});
+	});
+}
+
 
 var handler={};
 
@@ -37,13 +86,17 @@ function render(response,file){
 	});
 }
 
-function route(pathname,response,post){
+function route(pathname,response,get,post){
 	if(pathname.match(".html")){
 		render(response,pathname.slice(1));
 	}else if(pathname.match(".js")){
 		render(response,pathname.slice(1));
 	}else if(typeof handler[pathname] ==='function'){
-		handler[pathname](response,querystring.parse(post));
+		if(pathname==='info'){
+			handler[pathname](response,querystring.parse(get));
+		}else{
+			handler[pathname](response,querystring.parse(post));
+		}
 	}else{
 		response.end('not found');
 		console.log('unknow url:'+pathname);
@@ -58,7 +111,7 @@ function start(){
 			post+=chunk;
 		});
 		request.addListener('end',function(){
-			route(pathname,response,url.parse(request.url).query);
+			route(pathname,response,url.parse(request.url).query,post);
 		});
 	}).listen(8091);
 	console.log('server start');
@@ -66,4 +119,5 @@ function start(){
 
 
 handler['/info']=info;
+handler['/node_list']=node_list;
 ds.load_data('20120424.sty',start);
